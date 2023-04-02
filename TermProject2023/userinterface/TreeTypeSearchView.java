959f6c9fd1b412b8d10b39f4871d0a07db9233e3
Nathan Cummings <cnathan3030@gmail.com>
2023-04-02 14:52:15 -0400

    Nathan, 2:20 - 2:50, Worked on TreeTypeSearchView and UpdateTreeTypeView

--- TermProject2023/userinterface/TreeTypeSearchView.java
+++ TermProject2023/userinterface/TreeTypeSearchView.java
@@ -1,17 +1,16 @@
+// specify the package
 package userinterface;
 
 // system imports
-import javafx.event.Event;
+
+import impresario.IModel;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -21,57 +20,57 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
-import javafx.stage.Stage;
-import model.TreeType;
+import javafx.scene.control.*;
 
-import java.util.Properties;
-
-// project imports
-import impresario.IModel;
-
-public class UpdateTreeTypeView extends View
+/** The class containing the TreeTypeSearchView  for the Scout application */
+//==============================================================
+public class TreeTypeSearchView extends View
 {
-    // GUI components
-    protected TextField TypeDescriptionField;
-	protected TextField CostField;
+
+	// GUI components
 	protected TextField BarcodePrefixField;
 
 	protected Button cancelButton;
 	protected Button submitButton;
 
-    // For showing error message
+	// For showing error message
 	protected MessageView statusLog;
 
-    // constructor for this class -- takes a model object
+	// constructor for this class -- takes a model object
 	//----------------------------------------------------------
-    public UpdateTreeTypeView(IModel treeType)
-    {
-        super(treeType, "UpdateTreeTypeView");
+	public TreeTypeSearchView(IModel treeType)
+	{
+		super(treeType, "TreeTypeSearchView");
 
-        // create a container for showing the contents
+		// create a container for showing the contents
 		VBox container = new VBox(10);
 		container.setPadding(new Insets(15, 5, 5, 5));
 
-		// create our GUI components, add them to this panel
+		// Add a title for this panel
 		container.getChildren().add(createTitle());
+		
+		// create our GUI components, add them to this Container
 		container.getChildren().add(createFormContent());
 
-		// Error message area
-		container.getChildren().add(createStatusLog("                                            "));
+		container.getChildren().add(createStatusLog("             "));
 
 		getChildren().add(container);
-		
-		populateFields();
-    }
 
-    // Create the title container
+		populateFields();
+
+		myModel.subscribe("ServiceCharge", this);
+		myModel.subscribe("UpdateStatusMessage", this);
+	}
+
+
+	// Create the title container
 	//-------------------------------------------------------------
 	private Node createTitle()
 	{
 		HBox container = new HBox();
 		container.setAlignment(Pos.CENTER);	
 
-		Text titleText = new Text(" Boy Scout Troop 209 Tree Sales: Update TreeType ");
+		Text titleText = new Text(" Boy Scout Troop 209 Tree Sales: Search TreeType ");
 		titleText.setFont(Font.font("Arial", FontWeight.BOLD, 20));
 		titleText.setWrappingWidth(300);
 		titleText.setTextAlignment(TextAlignment.CENTER);
@@ -81,55 +80,37 @@ public class UpdateTreeTypeView extends View
 		return container;
 	}
 
-    // ----------------------------------------------------------------------
-    private VBox createFormContent() 
-    {
-        VBox vbox = new VBox(10);
+	// Create the main form content
+	//------------------------------------------------------------
+	private VBox createFormContent()
+	{
+		VBox vbox = new VBox(10);
 
 		GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
        	grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
-
-        Text prompt = new Text("TreeType Information");
+        
+        Text prompt = new Text("Search TreeType");
         prompt.setWrappingWidth(400);
         prompt.setTextAlignment(TextAlignment.CENTER);
         prompt.setFill(Color.BLACK);
         grid.add(prompt, 0, 0, 2, 1);
 
-		Text TypeDescriptionLabel = new Text(" Type Description : ");
-		Font myFont = Font.font("Helvetica", FontWeight.BOLD, 12);
-		TypeDescriptionLabel.setFont(myFont);
-		TypeDescriptionLabel.setWrappingWidth(150);
-		TypeDescriptionLabel.setTextAlignment(TextAlignment.RIGHT);
-		grid.add(TypeDescriptionLabel, 0, 1);
-
-		TypeDescriptionField = new TextField();
-		TypeDescriptionField.setEditable(false);
-		grid.add(TypeDescriptionField, 1, 1);
-
-		Text CostLabel = new Text(" Cost : ");
-		CostLabel.setFont(myFont);
-		CostLabel.setWrappingWidth(150);
-		CostLabel.setTextAlignment(TextAlignment.RIGHT);
-		grid.add(CostLabel, 0, 2);
-
-		CostField = new TextField();
-		CostField.setEditable(true);
-		grid.add(CostField, 1, 2);
-
+		// Find TreeType with a BarcodePrefix like
 		Text BarcodePrefixLabel = new Text(" Barcode Prefix : ");
+		Font myFont = Font.font("Helvetica", FontWeight.BOLD, 12);
 		BarcodePrefixLabel.setFont(myFont);
 		BarcodePrefixLabel.setWrappingWidth(150);
 		BarcodePrefixLabel.setTextAlignment(TextAlignment.RIGHT);
-		grid.add(BarcodePrefixLabel, 0, 3);
+		grid.add(BarcodePrefixLabel, 0, 1);
 
 		BarcodePrefixField = new TextField();
-		BarcodePrefixField.setEditable(false);
-		grid.add(BarcodePrefixField, 1, 3);
+		BarcodePrefixField.setEditable(true);
+		grid.add(BarcodePrefixField, 1, 1);
 
-		submitButton = new Button("Submit");
+		submitButton = new Button("Search");
 		submitButton.setFont(Font.font("Arial", FontWeight.BOLD, 14));
 		submitButton.setOnAction(new EventHandler<ActionEvent>() {
 
@@ -140,7 +121,9 @@ public class UpdateTreeTypeView extends View
 			}
 		});
 
-		cancelButton = new Button("Cancel");
+		HBox btnCont = new HBox(10);
+		btnCont.setAlignment(Pos.CENTER);
+		cancelButton = new Button("Back");
 		cancelButton.setFont(Font.font("Arial", FontWeight.BOLD, 14));
 		cancelButton.setOnAction(new EventHandler<ActionEvent>() {
 
@@ -151,18 +134,18 @@ public class UpdateTreeTypeView extends View
             	  }
         	});
 
-		HBox btnContainer = new HBox(10);
-		btnContainer.setAlignment(Pos.CENTER);
-		btnContainer.getChildren().add(cancelButton);
-		btnContainer.getChildren().add(submitButton);
+		btnCont.getChildren().add(cancelButton);
+		btnCont.getChildren().add(submitButton);
 	
 		vbox.getChildren().add(grid);
-		vbox.getChildren().add(btnContainer);
+		vbox.getChildren().add(btnCont);
 
 		return vbox;
-    }
+	}
 
-    // Create the status log field
+    /*
+     * Create the status log field
+     */
 	//-------------------------------------------------------------
 	protected MessageView createStatusLog(String initialMessage)
 	{
@@ -174,41 +157,16 @@ public class UpdateTreeTypeView extends View
 	//-------------------------------------------------------------
 	public void populateFields()
 	{
-        TreeType treeTypeToUpdate = (TreeType) myModel.getState("GetTreeTypeToUpdate");
-
-		TypeDescriptionField.setText(treeTypeToUpdate.getTypeDescription());
-		CostField.setText(treeTypeToUpdate.getCost());
-	 	BarcodePrefixField.setText(treeTypeToUpdate.getBarcodePrefix());
+		BarcodePrefixField.setText("");
 	}
 
-    public void processAction(Event evt)
-	{
-        if(CostField.getText().length() > 20)
-        {
-            displayErrorMessage("Cost has a limit of 25 characters!");
-        }
-        else if(Integer.parseInt(CostField.getText()) <= 0)
-        {
-            displayErrorMessage("Cost must be above $0!");
-        }
-		else
-		{
-			UpdateTreeType();
-		}
-	}
-
-    private void UpdateTreeType()
-	{
-		myModel.stateChangeRequest("UpdateTreeTypeData", CostField.getText());
-		displayMessage("TreeType Updated!");
-	}
-
-    /**
+	/**
 	 * Update method
 	 */
 	//---------------------------------------------------------
 	public void updateState(String key, Object value)
 	{
+
 	}
 
 	/**
@@ -238,5 +196,20 @@ public class UpdateTreeTypeView extends View
 		statusLog.clearErrorMessage();
 	}
 
-    
+    /*
+     * Process the entered BarcodePrefix
+     */
+    //----------------------------------------------------------
+	private void processAction(Event e)
+	{
+		if(BarcodePrefixField.getText().isEmpty())
+		{
+			displayErrorMessage("Please enter a Barcode Prefix");
+		}
+		else
+		{
+			myModel.stateChangeRequest("SearchTreeTypes", BarcodePrefixField.getText());
+		}
+	}
+
 }
